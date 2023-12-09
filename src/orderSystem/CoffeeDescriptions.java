2af218c509a2097b416cc0ac199daccf4e3b3169
JohnxdLink <JohnxdLink@gmail.com>
2023-12-09 12:37:34 +0800

    Msg: Add All Files

---
+++ src/orderSystem/CoffeeDescriptions.java
@@ -0,0 +1,79 @@
+package orderSystem;
+
+public class CoffeeDescriptions {
+	private String blkCoffeeLbl = "BLACK COFFEE";
+	private String imgBlkCoffee = "/orderSystem/images/coffees/Black Coffee Price 200px.PNG";
+	private String blkCoffeeDscrpt = "Pure and unadorned, black coffee is a bold, robust brew that lets the natural flavors of the coffee beans shine through.";
+	
+	private String latteLbl = "LATTE";
+	private String imgLatte = "/orderSystem/images/coffees/Latte Coffee Price 200px.PNG";
+	private String latteDscrpt = "A harmonious blend of espresso and steamed milk, a latte offers a creamy and customizable coffee experience, perfect for those who enjoy a smoother cup.";
+	
+	private String cappuccinoLbl = "CAPPUCCINO";
+	private String imgCappuccino = "/orderSystem/images/coffees/Cappuccino Coffee Price 200px.PNG";
+	private String cappuccinoDscrpt = "A luxurious mix of espresso, steamed milk, and frothed milk, a cappuccino provides a rich and velvety texture in every sip.";
+	
+	private String americanoLbl = "AMERICANO";
+	private String imgAmericano = "/orderSystem/images/coffees/Americano Coffee Price 200px.PNG";
+	private String americanoDscrpt = "A milder alternative to black coffee, made by diluting espresso with hot water, offering a balanced and flavorful cup.";
+	
+	private String espressoLbl = "ESPRESSO";
+	private String imgEspresso = "/orderSystem/images/coffees/Espresso Coffee 200px.PNG";
+	private String espressoDscrpt = "The essence of coffee in a concentrated form, espresso is a strong and intense shot with a distinct flavor profile and a golden crema topping.";
+	
+	// Black Coffee
+	public String getBlkCoffeeLbl() {
+		return blkCoffeeLbl;
+	}
+	public String getImgBlkCoffee() {
+		return imgBlkCoffee;
+	}	
+	public String getBlkCoffeeDscrpt() {
+		return blkCoffeeDscrpt;
+	}
+	
+	// Latte
+	public String getLatteLbl() {
+		return latteLbl;
+	}	
+	public String getImgLatte() {
+		return imgLatte;
+	}	
+	public String getLatteDscrpt() {
+		return latteDscrpt;
+	}
+	
+	// Cappuccino
+	public String getCappuccinoLbl() {
+		return cappuccinoLbl;
+	}	
+	public String getImgCappuccino() {
+		return imgCappuccino;
+	}	
+	public String getCappuccinoDscrpt() {
+		return cappuccinoDscrpt;
+	}
+	
+	// Americano
+	public String getAmericanoLbl() {
+		return americanoLbl;
+	}
+	public String getImgAmericano() {
+		return imgAmericano;
+	}	
+	public String getAmericanoDscrpt() {
+		return americanoDscrpt;
+	}
+	
+	// Espresso
+	public String getEspressoLbl() {
+		return espressoLbl;
+	}	
+	public String getImgEspresso() {
+		return imgEspresso;
+	}	
+	public String getEspressoDscrpt() {
+		return espressoDscrpt;
+	}
+	
+}
